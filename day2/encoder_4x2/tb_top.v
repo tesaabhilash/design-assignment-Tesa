31bfc24cf19113cd1be7b79ef451bd57f2236856
tesaabhilash <tesaabhilash@gmail.com>
2026-06-09 21:52:50 +0530

    Update tb_top.v

--- day2/encoder_4x2/tb_top.v
+++ day2/encoder_4x2/tb_top.v
@@ -1,1 +1,19 @@
-
+module encoder_4x2_tb();
+reg [3:0]d_tb;
+wire [1:0]b_tb;
+encoder_4x2 dut(.d(d_tb),.b(b_tb));
+integer m;
+initial
+begin
+d_tb=4'b0000;
+end
+initial
+begin
+for(m=0;m<4;m=m+1)
+begin
+#10;
+$monitor("value of d is %b value of b is %b",d_tb,b_tb);
+d_tb=4'b0000;
+d_tb[m]=1'b1;
+end
+end
