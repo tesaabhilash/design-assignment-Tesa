7f7589eaf754602065adc956e5de009c245dfb05
tesaabhilash <tesaabhilash@gmail.com>
2026-06-09 22:11:30 +0530

    Update top.v

--- day2/d_ff/design/top.v
+++ day2/d_ff/design/top.v
@@ -1,1 +1,20 @@
+module d_ff(input d,clk,rst,
+output reg q,qbar);
+always@(posedge clk)
+begin
+if (rst)
+begin 
+q<=1'b0;
+qbar<=1'b1;
+end
+else if (d==0)begin
+q<=1'b0;
+qbar<=1'b1;
+end
+else if (d==1)begin
+q<=1'b1;
+qbar<=1'b0;
+end
+end
 
+endmodule
