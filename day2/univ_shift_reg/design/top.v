fa6753990233bd91dc878f2bb25a9ed6aceee469
tesaabhilash <tesaabhilash@gmail.com>
2026-06-09 22:02:04 +0530

    Update top.v

--- day2/univ_shift_reg/design/top.v
+++ day2/univ_shift_reg/design/top.v
@@ -1,1 +1,27 @@
+module univ_reg(input clk,rst,sin,shift,load,mod,
+input[3:0]pin,
+output reg sout,
+output reg [3:0]pout );
+always@(posedge clk)
+begin
+if (rst) begin
+  sout<=1'b0;
+  pout<=4'b0000;
+end               
+else if (load) begin
+pout<=pin;
+sout<=1'b0;
+end
+else if (shift)begin
+if(mod)begin
+pout<={pin[2:0],sin};
+sout<=pout[3];
+end
+else begin
+pout<={sin,pin[2:0]};
+sout<=pout[0];
+end
+end
+end
+endmodule
 
