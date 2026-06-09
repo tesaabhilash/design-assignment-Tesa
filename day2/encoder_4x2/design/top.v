f8c14c2e061e0660537e83743963141d5c5483e1
tesaabhilash <tesaabhilash@gmail.com>
2026-06-09 21:52:02 +0530

    Update top.v

--- day2/encoder_4x2/design/top.v
+++ day2/encoder_4x2/design/top.v
@@ -1,1 +1,12 @@
-
+module encoder_4x2(input [3:0]d,
+output reg [1:0]b);
+always@(*)
+begin 
+case(d)
+4'b0001:b=2'b00;
+4'b0010:b=2'b01;
+4'b0100:b=2'b10;
+4'b1000:b=2'b11;
+default:b=2'b00;
+endcase
+end
