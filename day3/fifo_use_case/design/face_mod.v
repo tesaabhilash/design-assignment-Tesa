8bf444f269a9c0b06ca4193eff1ebdfdd01adad5
tesaabhilash <tesaabhilash@gmail.com>
2026-06-10 21:44:00 +0530

    Update face_mod.v

--- day3/fifo_use_case/design/face_mod.v
+++ day3/fifo_use_case/design/face_mod.v
@@ -1,1 +1,16 @@
+module face_mod(
+    input clk,
+    input rst,
+    input sin,
+    input [3:0] key,
+    output reg sout
+);
 
+always @(posedge clk) begin
+    if(rst)
+        sout <= 0;
+    else
+        sout <= sin;
+end
+
+endmodule
