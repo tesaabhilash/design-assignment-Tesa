a62afe66ca56f8c29f39fa900965f20e34dca80d
tesaabhilash <tesaabhilash@gmail.com>
2026-06-11 21:55:11 +0530

    Update top.v

--- day4/block_mem_gen/design/top.v
+++ day4/block_mem_gen/design/top.v
@@ -1,7 +1,7 @@
 module block_mem_gen(input clk,arstn,wrenb,
 input [7:0]din,wraddress,rdaddress,
 output reg [7:0]dout);
-reg [7:0] mem [0:255];  // 256 locations, each 8 bits wide
+ reg [7:0] mem [7:0];  // 256 locations, each 8 bits wide
 integer i;
 
 always@(posedge clk or negedge arstn)begin
@@ -15,7 +15,7 @@ if (arstn) begin
  end
  end
  else begin
- for(i=0;i<256;i=i+1)
+  for(i=0;i<8;i=i+1)
     mem[i]<=0;
 end
 end
